8227d886ad3df58810a31e8eb3bd7241a58089c9
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2021-12-02 11:41:01 +0100

    google ana anpassung

--- function.js
+++ function.js
@@ -4,11 +4,11 @@ window.onload=checkcookie();
 
 
 function starttracking(){
-	 window.dataLayer = window.dataLayer || [];
+	window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-HJTCFGZ87K');
+    gtag('config', 'G-HJTCFGZ87K');
 	/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -23,12 +23,12 @@ ga('send', 'pageview');*/
 
 
  function checkcookie() {
-	 if (document.cookie.indexOf("nocookie") <= 0) {
+	if (document.cookie.indexOf("nocookie") <= 0) {
     //new visit
 	
 	
   }else{
-	  
+
 	//returning
 	var containerelement = document.getElementById("container");
 	containerelement.classList.remove("active");
